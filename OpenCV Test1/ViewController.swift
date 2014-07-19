dfef1d08498535a4f99ed0dbe11c6035a320d7cd
Shin <shin@murase-no-MacBook-Pro.local>
2014-07-19 21:52:39 +0900

    Initial Commit

---
+++ OpenCV Test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenCV Test1
+//
+//  Created by Shin on 2014/07/19.
+//  Copyright (c) 2014年 shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ OpenCV Test1Tests/OpenCV_Test1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  OpenCV_Test1Tests.swift
+//  OpenCV Test1Tests
+//
+//  Created by Shin on 2014/07/19.
+//  Copyright (c) 2014年 shin. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class OpenCV_Test1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
